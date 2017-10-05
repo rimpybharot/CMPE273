de0866d59cb30be868b5cae5aab7526b09655ce9
rimpybharot <rbverses21@gmail.com>
2017-10-04 19:45:26 -0700

    Add comments in server file

--- lab1/server.py
+++ lab1/server.py
@@ -22,13 +22,27 @@ class MyDatastoreServicer(datastore_pb2.DatastoreServicer):
     def put(self, request, context):
         print("put")
         key = uuid.uuid4().hex
-        # TODO - save key and value into DB converting request.data string to utf-8 bytes 
+        # save key and value into DB converting request.data string to utf-8 bytes 
+        '''Rocksdb works on bytes hence it can not comprehend the string.
+        First we need to extract the data from the request which is a string,
+        and then we use encodings libraary to encode it to bytes.
+        Finally provide the encoded byte value to the db , alongwith the encoded value
+        of the key. Do db.put to add the key-value into the database.
+        '''
         self.db.put(key.encode(), request.data.encode());
         return datastore_pb2.Response(data=key)
 
     def get(self, request, context):
         print("get")
-        # TODO - retrieve the value from DB by the given key. Needs to convert request.data string to utf-8 bytes. 
+        # retrieve the value from DB by the given key. Needs to convert request.data string to utf-8 bytes. 
+        '''Rocksdb works on bytes hence it can not comprehend the string.
+        First we need to extract the data from the request which is a string,
+        and then we use encodings libraary to encode it to bytes.
+        Finally provide the encoded byte value to the db , alongwith the encoded value
+        of the key. Do db.get to get the value from the database for the key provided by user.
+        The value returned will be in bytes and hence we need to decode it to the string.
+        We use encodings library to decode the value retrieved from the data and send it as response.
+        '''
         value = (self.db.get(request.data.encode())).decode();
         return datastore_pb2.Response(data=value)
 
